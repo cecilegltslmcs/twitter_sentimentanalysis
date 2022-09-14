3a6967fe7ba031155db58f1cfd4eafff224812e0
Aurélien Blanc <au.blanc@outlook.fr>
2022-09-14 12:27:51 +0000

    Update producer.py to match api v2

--- app/producer.py
+++ app/producer.py
@@ -4,35 +4,30 @@ import logging
 
 """API ACCESS KEYS"""
 
-consumerKey = "consumerKey"
-consumerSecret = "consumerSecret"
-accessToken = "accessToken"
-accessTokenSecret = "accessTokenSecret"
-
+# consumerKey = "consumerKey"
+# consumerSecret = "consumerSecret"
+# accessToken = "accessToken"
+# accessTokenSecret = "accessTokenSecret"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAADFpgwEAAAAApY9f2nrgrD1MzpC6G4ln7Oo5HCo%3DzI0bRiULFsBubjn6MX5fyLb4FbFDdQQQz6gS5yRI9NeDPPQvFM"
 ip_server = "51.38.185.58:9092" 
 
 producer = KafkaProducer(bootstrap_servers=ip_server)
 search_term = 'ClimateCrisis'
 topic_name = 'twitter'
 
-client = tweepy.Client(
-    consumer_key = consumerKey,
-    consumer_secret = consumerSecret,
-    access_token = accessToken,
-    access_token_secret = accessTokenSecret
-)
+client = tweepy.Client(bearer_token=bearer_token)
 
 def twitterAuth():
     # create the authentication object
-    authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
-    # set the access token and the access token secret
-    authenticate.set_access_token(accessToken, accessTokenSecret)
+    authenticate = tweepy.OAuth2BearerHandler(bearer_token)
+    ## set the access token and the access token secret
+    #authenticate.set_access_token(accessToken, accessTokenSecret)
     # create the API object
     api = tweepy.API(authenticate, wait_on_rate_limit=True)
     return api
 
 
-class TweetListener(tweepy.Stream):
+class TweetListener(tweepy.StreamingClient):
 
     def on_data(self, raw_data):
         logging.info(raw_data)
@@ -45,10 +40,13 @@ class TweetListener(tweepy.Stream):
             return False
 
     def start_streaming_tweets(self, search_term):
-        self.filter(track=search_term, stall_warnings=True, languages=["en"])
+        self.add_rules(tweepy.StreamRule(search_term))
+        self.filter()
+        #self.filter(track=search_term, stall_warnings=True, languages=["en"])
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    twitter_stream = TweetListener(consumerKey, consumerSecret, accessToken, accessTokenSecret)
+    #init twitter_stream class
+    twitter_stream = TweetListener(bearer_token)
+        
     twitter_stream.start_streaming_tweets(search_term)
