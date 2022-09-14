83e939c0d45f51f876d4f341288a229aa8a1a5f7
Cecile Guillot <cecile.guillot@live.fr>
2022-09-14 18:37:43 +0200

    Update consumer with MongoDB

--- app/consumer.py
+++ app/consumer.py
@@ -1,9 +1,18 @@
 from kafka import KafkaConsumer
+from pymongo import MongoClient
 import json
+
+# Connect to MongoDB and database
+try:
+   client = MongoClient('localhost', 27017)
+   db = client.raw_tweet
+   print("Connected successfully!")
+except:  
+   print("Could not connect to MongoDB")
+    
 
 topic_name = 'twitter-mac'
 ip_server = "51.38.185.58:9092"
-
 
 consumer = KafkaConsumer(
     topic_name,
@@ -15,6 +24,17 @@ consumer = KafkaConsumer(
      max_poll_records=100,
      value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
-for message in consumer:
-    tweets = json.loads(json.dumps(message.value))
-    print(tweets)
+
+# Parse received data from Kafka
+for msg in consumer:
+    record = json.loads(json.dumps(msg.value))
+    print(record)
+    data = record['data']
+    
+    # Create dictionary and ingest data into MongoDB
+    try:
+       tweet_rec = {'data':data}
+       rec_id1 = db.coba_info.insert_one(tweet_rec)
+       print("Data inserted with record ids", rec_id1)
+    except:
+       print("Could not insert into MongoDB")
