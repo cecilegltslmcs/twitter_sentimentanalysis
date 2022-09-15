aa4f4e2a505c871b226fe48f9a5ceb4af270cc4f
Cecile Guillot <cecile.guillot@live.fr>
2022-09-15 23:19:03 +0200

    Add counters & graphical modification

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,7 +8,7 @@ import auth_token as auth
 import time
 
 #connection to the cluster
-# Connect to MongoDB and database
+#connect to MongoDB and database
 
 @st.experimental_singleton
 def init_connection():
@@ -34,48 +34,41 @@ st.title("Twitter sentiment analysis using the keyword 'ClimateCrisis'")
 st.sidebar.title("Chart type and data")
 option = st.sidebar.selectbox("Choose your analysis.", ('Home','Pie Chart', 'Distribution Chart', 'Boxplot', 'Table'))
 
+if option == "Home":
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write(' ')
+    with col2:
+        st.image("Twitter-logo.png", width=150)
+    with col3:
+        st.write(' ')
+    st.subheader("Welcome to the dashboard about tweets sentiments related to climate crisis. In order to choose your analysis, select an option on the sidebar.")
+    st.image("pxclimateaction-g25a4b047f_1920.jpg")
+    st.write("Réalisé par Aurélien Blanc, Cécile Guillot & Matthieu Cavaillon")
+
 placeholder = st.empty()
-
-for seconds in range(2000):
+    
+while True:
     items = get_data()
-
+    
     with placeholder.container():
+    
         df = pd.DataFrame(items)
         df.drop("processed_text", axis=1, inplace=True)
         df = df.astype({'_id':str ,'text':str, 'polarity':float, 'sentiment':str})
 
-        if option == "Home":
-
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                st.write(' ')
-            with col2:
-                st.image("./img/Twitter-logo.png", width=150)
-            with col3:
-                st.write(' ')
-            
-            st.subheader("Welcome to the dashboard about tweets sentiments related to climate crisis. In order to choose your analysis, select an option on the sidebar.")
-            st.image("./img/pxclimateaction-g25a4b047f_1920.jpg")
-            st.write("Réalisé par Aurélien Blanc, Cécile Guillot & Matthieu Cavaillon")
-
         if option == 'Pie Chart':
             st.subheader("Distribution of the different sentiment in the tweets")
             sentiment_count = df.groupby(['sentiment']).agg(nb_sentiment=pd.NamedAgg(column="sentiment", aggfunc="count")).reset_index()
-
-            colors = ['red', 'darkorange', 'lightgreen']
-            fig = go.Figure(data=[go.Pie(
-                                    labels=sentiment_count.sentiment,
-                                    values=sentiment_count.nb_sentiment)])
-            fig.update_traces(hoverinfo='value', textinfo='label+percent', textfont_size=15,
-                            marker=dict(colors=colors))
+            colors = ['indianred', 'lightgray', 'lightgreen']
+            fig = go.Figure(data=[go.Pie(labels=sentiment_count.sentiment,values=sentiment_count.nb_sentiment)])
+            fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
+            fig.update_traces(hoverinfo='percent', textinfo='label+value', textfont_size=15, marker=dict(colors=colors))
             st.plotly_chart(fig)
 
         if option =="Distribution Chart":
             st.subheader("Number of tweets by polarity")
-            fig2 = px.histogram(df,
-                                x="polarity",
-                                opacity=0.7,
-                                log_y=True)
+            fig2 = px.histogram(df, x="polarity", opacity=0.7, log_y=True)
             st.plotly_chart(fig2)
 
         if option =="Boxplot":
@@ -83,18 +76,16 @@ for seconds in range(2000):
             y_positive = df.loc[df["sentiment"] == "Positive"]
             y_negative = df.loc[df["sentiment"] == "Negative"]
             fig3 = go.Figure()
-            fig3.add_trace(go.Box(y=y_negative.polarity, name='Negative',
-                        marker_color = 'red'))
-            fig3.add_trace(go.Box(y=y_neutral.polarity, name = 'Neutral',
-                        marker_color = 'darkorange'))
-            fig3.add_trace(go.Box(y=y_positive.polarity, name = 'Positive',
-                        marker_color = 'lightgreen'))
+            fig3.add_trace(go.Box(y=y_negative.polarity, name='Negative', marker_color = 'indianred'))
+            fig3.add_trace(go.Box(y=y_neutral.polarity, name = 'Neutral', marker_color = 'lightgray'))
+            fig3.add_trace(go.Box(y=y_positive.polarity, name = 'Positive', marker_color = 'lightgreen'))
             st.subheader("Visualisation of the statistics for each sentiment")
             st.plotly_chart(fig3)
 
         if option == "Table":
-            st.subheader('Visualize the tweets, the polarity compound and the sentiment')
+            counts = len(df.index)
+            st.subheader('Visualize the tweets, the polarity compound and the sentiments.')
+            st.write("Nombre de tweets:", counts)
             st.dataframe(df)
-            time.sleep(1)
         
         time.sleep(1)
