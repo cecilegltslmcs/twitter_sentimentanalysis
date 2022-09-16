6447a5103da1d839a3f2236833ad96fde9e94fde
Cecile Guillot <cecile.guillot@live.fr>
2022-09-16 11:45:46 +0200

    update

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from pyspark.sql import functions as F
 import auth_token as auth
-from pyspark.sql.types import StringType, StructType, StructField, FloatType
+from pyspark.sql.types import StringType, FloatType
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 import re
