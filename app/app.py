17503490fcc3e8bc059a5e3e0b00d673a8552c83
Cecile Guillot <cecile.guillot@live.fr>
2022-09-23 14:17:17 +0200

    modify code respecting pep

--- app/app.py
+++ app/app.py
@@ -65,26 +65,24 @@ def write_row(batch_df , batch_id):
     pass
 
 if __name__ == "__main__":
-    spark = SparkSession \
-        .builder \
-        .config("spark.mongodb.input.uri", auth.uri_mongo)\
-        .config("spark.mongodb.output.uri", auth.uri_mongo)\
-        .appName("TwitterSentimentAnalysis") \
-        .getOrCreate()
-    
+    spark = (SparkSession
+        .builder
+        .config("spark.mongodb.input.uri", auth.uri_mongo)
+        .config("spark.mongodb.output.uri", auth.uri_mongo)
+        .appName("TwitterSentimentAnalysis")
+        .getOrCreate())
     sc = spark.sparkContext.setLogLevel("ERROR")
 
     raw_json = spark.read.json("tweet.json",\
                                 multiLine=True)
-
-    df = spark \
-        .readStream \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", ip_server)\
-        .option("subscribe", topic_name) \
-        .load()\
-        .withColumn("value", from_json(col("value").cast("string"), raw_json.schema))\
-        .select(col('value.data.text'))
+    df = (spark
+        .readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", ip_server)
+        .option("subscribe", topic_name)
+        .load()
+        .withColumn("value", from_json(col("value").cast("string"), raw_json.schema))
+        .select(col('value.data.text')))
 
     clean_tweets = F.udf(cleanTweet, StringType())
     raw_tweets = df.withColumn('processed_text', clean_tweets(col("text")))
@@ -96,29 +94,29 @@ if __name__ == "__main__":
     sentiment_tweets = polarity_tweets.withColumn("sentiment", sentiment(col("polarity")))
 
     """ # console display (debug mode)
-    query = sentiment_tweets\
-            .writeStream\
-            .queryName("test_tweets") \
-            .outputMode("append")\
-            .format("console")\
-            .start()\
-            .awaitTermination()"""
+    query = (sentiment_tweets
+            .writeStream
+            .queryName("test_tweets")
+            .outputMode("append")
+            .format("console")
+            .start()
+            .awaitTermination())"""
     
     """# parquet file dumping
     parquet = sentiment_tweets.repartition(1)
-    query2 = parquet \
-        .writeStream \
-        .queryName("final_tweets_parquet") \
-        .outputMode("append").format("parquet") \
-        .option("path", "./parquet_save") \
-        .option("checkpointLocation", "./check") \
-        .trigger(processingTime='60 seconds')\
-        .start()\
-        .awaitTermination()"""
+    query2 = (parquet
+        .writeStream
+        .queryName("final_tweets_parquet")
+        .outputMode("append").format("parquet")
+        .option("path", "./parquet_save")
+        .option("checkpointLocation", "./check")
+        .trigger(processingTime='60 seconds')
+        .start()
+        .awaitTermination())"""
 
     # mongodb dumping
-    query = sentiment_tweets\
-            .writeStream\
-            .foreachBatch(write_row)\
-            .start()\
-            .awaitTermination()
+    query = (sentiment_tweets
+            .writeStream
+            .foreachBatch(write_row)
+            .start()
+            .awaitTermination())
